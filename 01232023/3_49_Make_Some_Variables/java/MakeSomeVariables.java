767160fc85ba5de87487b32f83ebac21943c35bc
Peter Hardy <peterphardy@gmail.com>
2023-01-19 13:51:40 -0600

    First exercise completed in java

---
+++ 01232023/3_49_Make_Some_Variables/java/MakeSomeVariables.java
@@ -0,0 +1,19 @@
+public class MakeSomeVariables {
+
+    public static void main(String[] args) {
+        // Define a variable named city and set it equal to any string
+        String city = "Chicago";
+        // Define a variable named price and set it equal to any float
+        float price = 10.99f;
+        // Define a variable named high_score and set it equal to any int
+        int high_score = 5000;
+        // Define a variable named is_having_fun and set it to a Boolean value
+        boolean is_having_fun = true;
+
+        System.out.println(city);
+        System.out.println(price);
+        System.out.println(high_score);
+        System.out.println(is_having_fun);
+    }
+
+}
