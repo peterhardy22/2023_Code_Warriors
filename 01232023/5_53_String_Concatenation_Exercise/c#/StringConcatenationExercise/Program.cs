56fe89040ad514979e0b2c6e18e659d97a24f8c4
Peter Hardy <PHardy_a@fhlbc.com>
2023-01-19 12:34:25 -0600

    Third exercise completed in c#

---
+++ 01232023/5_53_String_Concatenation_Exercise/c#/StringConcatenationExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StringConcatenationExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var greeting = "Hello";
+            var name = "Hardy";
+            var greet_name = greeting + " " + name;
+
+            Console.WriteLine(greet_name);
+            Console.ReadLine();
+        }
+    }
+}
