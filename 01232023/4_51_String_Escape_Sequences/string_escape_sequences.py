69f2dfefe2ec0b9aee0ccda83b2a9198d124ac20
Peter Hardy <peterphardy@gmail.com>
2023-01-19 10:43:18 -0600

    Second exercise in Python completed

---
+++ 01232023/4_51_String_Escape_Sequences/string_escape_sequences.py
@@ -0,0 +1,13 @@
+ # Set the message variable equal to any string containing a new-line escape sequence
+message = "hello down there \n hello up there"
+
+# Add a string to the mountains variable that when printed results in: /\/\/\
+# You will need to use an escape sequence more than once!
+mountains = "/\\/\\/\\"
+
+# Set the quotation variable to any string that contains an escaped double quotation mark
+quotation = "\"Hey there!\" Said the dude."
+
+print(f'{message=}')
+print(f'{mountains=}')
+print(f'{quotation=}')
