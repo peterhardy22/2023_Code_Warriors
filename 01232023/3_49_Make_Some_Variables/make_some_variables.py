b76413ec9294548b1310dff88b1c0c4f80466648
Peter Hardy <peterphardy@gmail.com>
2023-01-19 10:42:58 -0600

    First exercise in Python completed

---
+++ 01232023/3_49_Make_Some_Variables/make_some_variables.py
@@ -0,0 +1,13 @@
+# Define a variable named city and set it equal to any string
+city = 'Chicago'
+# Define a variable named price and set it equal to any float
+price = 10.0
+# Define a variable named high_score and set it equal to any int
+high_score = 5000
+# Define a variable named is_having_fun and set it to a Boolean value
+is_having_fun = True
+
+print(f'{city=}')
+print(f'{price=}')
+print(f'{high_score=}')
+print(f'{is_having_fun=}')
