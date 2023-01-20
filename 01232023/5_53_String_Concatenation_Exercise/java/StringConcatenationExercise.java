56a9bc56f7e36061983fe1ac934bb995383ac7d5
Peter Hardy <peterphardy@gmail.com>
2023-01-19 22:16:08 -0600

    Third exercise completed in java

---
+++ 01232023/5_53_String_Concatenation_Exercise/java/StringConcatenationExercise.java
@@ -0,0 +1,9 @@
+public class StringConcatenationExercise {
+    public static void main(String[] args) {
+        String greeting = "Hello";
+        String name = "Hardy";
+        String greetName = greeting + " " + name;
+
+        System.out.println(greetName);
+    }
+}
