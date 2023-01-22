2723430c4281145069cd308e113251efefa2c6d9
Peter Hardy <peterphardy@gmail.com>
2023-01-22 14:41:01 -0600

    First exercise completed in java

---
+++ 01272023/6_56_Formatting_Strings/java/FormattingStrings.java
@@ -0,0 +1,8 @@
+public class FormattingStrings {
+    public static void main(String[] args) {
+        String firstName = "Peter";
+        String lastName = "Hardee";
+
+        System.out.println("First Name: " + firstName + ", Last Name: " + lastName);
+    }
+}
