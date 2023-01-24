542ec72ea37031e4838fae178262fd309d175e78
Peter Hardy <PHardy_a@fhlbc.com>
2023-01-23 22:27:41 -0600

    Third challenge completed in c#

---
+++ 01272023/08_64_Number_Is_Odd/c#/NumberIsOdd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberIsOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomizer = new Random();
+            int randomNumber = randomizer.Next(1, 1000);
+            Console.WriteLine(randomNumber);
+
+            if (randomNumber % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
