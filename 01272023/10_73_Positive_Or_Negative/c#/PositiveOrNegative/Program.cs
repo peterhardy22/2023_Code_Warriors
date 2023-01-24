50145681d8b01a44a9b4e49b40c9b56c921acd52
Peter Hardy <PHardy_a@fhlbc.com>
2023-01-23 22:55:53 -0600

    Fifth challenge completed in c#

---
+++ 01272023/10_73_Positive_Or_Negative/c#/PositiveOrNegative/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PositiveOrNegative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomizer = new Random();
+            int x = randomizer.Next(-100, 100);
+            int y = randomizer.Next(-100, 100);
+            Console.WriteLine("x: " + x);
+            Console.WriteLine("y: " + y);
+
+            if (x > 0 & y > 0)
+            {
+                Console.WriteLine("both positive");
+            }
+            else if (x < 0 & y < 0)
+            {
+                Console.WriteLine("both negative");
+            }
+            else if (x > 0 & y < 0)
+            {
+                Console.WriteLine("x is positive and y is negative");
+            }
+            else
+            {
+                Console.WriteLine("y is positive and x is negative");
+            }
+            Console.ReadLine();
+        }
+    }
+}
