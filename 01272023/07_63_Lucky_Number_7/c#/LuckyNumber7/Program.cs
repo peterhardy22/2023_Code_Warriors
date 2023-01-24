14622a6703f48dfc93c0cf9d2f1ac95b4ccaf0ce
Peter Hardy <PHardy_a@fhlbc.com>
2023-01-23 22:24:37 -0600

    Second challenge completed in c#

---
+++ 01272023/07_63_Lucky_Number_7/c#/LuckyNumber7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckyNumber7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomizer = new Random(); 
+            int choice = randomizer.Next(1, 10);
+            Console.WriteLine(choice);
+
+            if (choice == 7)
+            {
+              Console.WriteLine("lucky");
+            }
+            else 
+            {
+              Console.WriteLine("unlucky");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
