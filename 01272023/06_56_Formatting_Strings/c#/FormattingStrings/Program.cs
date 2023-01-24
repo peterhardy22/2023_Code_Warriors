efa1cdee227e15151cb62f1a3ff64698cd3ea99c
Peter Hardy <PHardy_a@fhlbc.com>
2023-01-23 22:16:39 -0600

    First exercise completed in c#

---
+++ 01272023/06_56_Formatting_Strings/c#/FormattingStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormattingStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Peter";
+            var lastName = "Hardee";
+
+            Console.WriteLine("First Name: " + firstName + ", Last Name: " + lastName);
+            Console.ReadLine();
+        }
+    }
+}
