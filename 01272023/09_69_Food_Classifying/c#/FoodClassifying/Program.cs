08666d39036654d7d8ec7b7ab090cc5f3f2542ee
Peter Hardy <PHardy_a@fhlbc.com>
2023-01-23 22:45:10 -0600

    Third challenge completed in c#

---
+++ 01272023/09_69_Food_Classifying/c#/FoodClassifying/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodClassifying
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var foodList = new List<string>()
+                    {
+                        "apple",
+                        "grape",
+                        "bacon",
+                        "steak",
+                        "worm",
+                        "dirt"
+                    };
+
+            var random = new Random();
+            int index = random.Next(foodList.Count);
+            var food = foodList[index];
+            Console.WriteLine(food);
+
+            if (food == "apple" || food == "grape")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (food == "bacon" || food == "steak")
+            {
+                Console.WriteLine("meat");
+            }
+            else if (food == "dirt" || food == "worm")
+            {
+                Console.WriteLine("yuck");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
