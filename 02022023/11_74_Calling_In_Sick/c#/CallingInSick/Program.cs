69ce5f0fea45e9bd28c0dbe2338496dc0adc36b0
Peter Hardy <PHardy_a@fhlbc.com>
2023-02-03 10:26:04 -0600

    Fourth challenge completed in c#

---
+++ 02022023/11_74_Calling_In_Sick/c#/CallingInSick/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingInSick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomizer = new Random();
+
+            bool actuallySick = randomizer.NextDouble() < 0.5;
+            bool kindaSick = randomizer.NextDouble() < 0.5;
+            bool hateYourJob = randomizer.NextDouble() < 0.5;
+            int sickDays = randomizer.Next(1, 10);
+
+            if ((actuallySick && sickDays > 0) | (kindaSick && hateYourJob && sickDays > 0)) {
+                bool callingInSick = true;
+                Console.WriteLine("It is " + callingInSick + ", I will be calling in sick today!");
+            } else
+            {
+                bool callingInSick = false;
+                Console.WriteLine("It is " + callingInSick + ", I will not be calling in sick today!");
+            };
+            Console.ReadLine();
+        }
+
+    }
+}
