5348ac09e9719be0b7735b560a36ce7a7e6e7298
Peter Hardy <peterphardy@gmail.com>
2023-02-02 22:49:53 -0600

    Fourth challenge completed in java

---
+++ 02022023/04_08_Subtract_The_Sum/java/SubtractTheSum.java
@@ -0,0 +1,9 @@
+public class SubtractTheSum {
+    public static void main(String[] args) {
+        subtractSum(10);
+    }
+
+    public static String subtractSum (int n) {
+        return "apple";
+    }
+}
