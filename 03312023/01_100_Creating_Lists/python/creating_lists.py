26d84ed832728819ddf1cae45ae1725f2ea48ada
Peter Hardy <peterphardy@gmail.com>
2023-03-31 22:57:07 -0500

    little push

--- 03312023/01_100_Creating_Lists/python/creating_lists.py
+++ 03312023/01_100_Creating_Lists/python/creating_lists.py
@@ -1,5 +1,5 @@
 # Define my_stuff 
-my_stuff: list = ["something", "something else", "this", "that", 2.0]
+my_stuff: list = ["something", "something else", "this", "that", 1, 2.0, 3.004]
 
 # Define nums 
 nums: list = list(range(1, 100))
