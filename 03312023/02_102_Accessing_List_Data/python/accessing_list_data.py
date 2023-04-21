3ab59758a99847c38a7055bdb405daa9e7b93ec7
Peter Hardy <peterphardy@gmail.com>
2023-04-21 13:22:07 -0500

    exercise 2 completed in python

---
+++ 03312023/02_102_Accessing_List_Data/python/accessing_list_data.py
@@ -0,0 +1,8 @@
+people: list = ["Hanna", "Louisa", "Claudia", "Angela", "Geoffrey", "aparna"]
+
+# Change "Hanna" to "Hannah".
+people[0] = "Hannah"
+# Change "Geoffrey" to "Jeffrey".
+people[4] = "Jeffrey"
+# Change "aparna" to "Aparna" (capitalize it).
+people[-1] = "Aparna"
