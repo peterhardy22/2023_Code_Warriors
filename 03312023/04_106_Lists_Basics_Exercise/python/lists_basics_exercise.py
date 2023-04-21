1afaa47bf075c528a8ad8d22497aff449329bbef
Peter Hardy <peterphardy@gmail.com>
2023-04-21 13:28:18 -0500

    Exercise 4 completed in python

---
+++ 03312023/04_106_Lists_Basics_Exercise/python/lists_basics_exercise.py
@@ -0,0 +1,8 @@
+# Initially create an empty list called instructors
+instructors = []
+
+# Add the following strings to the instructors list 
+    # "Colt"
+    # "Blue"
+    # "Lisa"
+instructors.extend(["Colt", "Blue", "Lisa"])
