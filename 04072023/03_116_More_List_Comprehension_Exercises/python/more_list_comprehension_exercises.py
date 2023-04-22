2a432fa96326099f46b315c4c7947d1a42fec57b
Peter Hardy <peterphardy@gmail.com>
2023-04-21 22:01:36 -0500

    Third exercise completed in python

---
+++ 04072023/03_116_More_List_Comprehension_Exercises/python/more_list_comprehension_exercises.py
@@ -0,0 +1,5 @@
+first_answer: list = [number for number in [1,2,3,4] if number in [3,4,5,6]]
+print(first_answer)
+
+second_answer: list = [name[::-1].lower() for name in ["Elie", "Tim", "Matt"]]
+print(second_answer)
