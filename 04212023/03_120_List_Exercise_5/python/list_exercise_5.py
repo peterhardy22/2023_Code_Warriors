b140e0acf6a46b0a801cb15f7f29b812bdb3811a
Peter Hardy <peterphardy@gmail.com>
2023-04-22 21:56:28 -0500

    Fourth exercise completed in python

---
+++ 04212023/03_120_List_Exercise_5/python/list_exercise_5.py
@@ -0,0 +1,2 @@
+answer: list = [[number for number in range(0, 3)] for item in range(0, 3)]
+print(f"{answer=}")
---
+++ 04212023/04_121_One_More_Nested_List_Comp_Challenge/python/one_more_nested_list_comp_challenge.py
@@ -0,0 +1,2 @@
+answer: list = [[number for number in range(0, 10)] for item in range(0, 10)]
+print(f"{answer=}")
