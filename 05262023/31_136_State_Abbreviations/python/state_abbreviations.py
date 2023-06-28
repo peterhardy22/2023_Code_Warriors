d675f51768d14bae745af06130233fe91c132f30
Peter Hardy <peterphardy@gmail.com>
2023-06-27 21:30:33 -0500

    Challenge 1 completed in python

---
+++ 05262023/31_136_State_Abbreviations/python/state_abbreviations.py
@@ -0,0 +1,7 @@
+list1: list = ["CA", "NJ", "RI"]
+list2: list = ["California", "New Jersey", "Rhode Island"]
+
+# make sure your solution is assigned to the answer variable so the tests can work!
+answer: dict = dict(zip(list1, list2))
+
+print(answer)
