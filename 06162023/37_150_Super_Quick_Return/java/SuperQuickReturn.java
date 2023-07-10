2f8d4b5a887d67bea296975aa887db9cc473743c
Peter Hardy <peterphardy@gmail.com>
2023-07-09 22:02:04 -0500

    Challenge completed in java

---
+++ 06162023/37_150_Super_Quick_Return/java/SuperQuickReturn.java
@@ -0,0 +1,9 @@
+public class SuperQuickReturn {
+    public static void main(String[] args) {
+        System.out.println(speakPig());
+    }   
+    
+    public static String speakPig() {
+        return "oink";
+    }
+}
