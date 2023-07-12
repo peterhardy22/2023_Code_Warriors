50248811c6ce0874b2924e8be1132ac7e5d37939
Peter Hardy <peterphardy@gmail.com>
2023-07-12 14:55:13 -0500

    Improved to take in arrays so no 2 element limit

--- 06162023/35_143_Tuples_And_Sets/java/Tuple.java
+++ 06162023/35_143_Tuples_And_Sets/java/Tuple.java
@@ -1,25 +1,15 @@
-public class Tuple<T1, T2> {
-    private T1 first;
-    private T2 second;
+public class Tuple<T> {
+    private T[] elements;
 
-    public Tuple(T1 first, T2 second) {
-        this.first = first;
-        this.second = second;
+    public Tuple(T[] elements) {
+        this.elements = elements;
     }
 
-    public T1 getFirst() {
-        return first;
+    public T[] getElements() {
+        return elements;
     }
 
-    public void setFirst(T1 first) {
-        this.first = first;
-    }
-
-    public T2 getSecond() {
-        return second;
-    }
-
-    public void setSecond(T2 second) {
-        this.second = second;
+    public static <T> Tuple<T> arrayToTuple(T[] array) {
+        return new Tuple<>(array);
     }
 }
