f3003bab5a9e4f7d38a2b8b96bf83923e1a9641e
Peter Hardy <peterphardy@gmail.com>
2023-07-11 21:28:38 -0500

    function challenge completed in java

---
+++ 06162023/36_147_First_Function/java/FirstFunction.java
@@ -0,0 +1,9 @@
+public class FirstFunction {
+    public static void main(String[] args) {
+        makeNoise();
+    }
+
+    public static void makeNoise() {
+        System.out.println("THE CROWD GOES WILD");
+    }
+}
