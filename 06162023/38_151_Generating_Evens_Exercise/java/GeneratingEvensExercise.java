c28dfdfaa3925025c83d146c5327fa387f81f2d7
Peter Hardy <peterphardy@gmail.com>
2023-07-14 09:16:57 -0500

    More dynamic w/ no array length limit w/ ArrayList

--- 06162023/38_151_Generating_Evens_Exercise/java/GeneratingEvensExercise.java
+++ 06162023/38_151_Generating_Evens_Exercise/java/GeneratingEvensExercise.java
@@ -1,23 +1,21 @@
+import java.util.ArrayList;
+
 public class GeneratingEvensExercise {
     public static void main(String[] args) {
-        int upperLimit = 50; // Example upper limit
-        int[] evenNumbers = generateEvens(upperLimit);
+        int upperLimit = 50;
+        ArrayList<Integer> evenNumbers = generateEvens(upperLimit);
 
-        // Access and use the even numbers array as needed
         for (int number : evenNumbers) {
             System.out.println(number);
         }
     }
 
-    public static int[] generateEvens(int upperLimit) {
-        int arraySize = (upperLimit / 2) - 1;
-        int[] numbers = new int[arraySize];
-        int index = 0;
+    public static ArrayList<Integer> generateEvens(int upperLimit) {
+        ArrayList<Integer> numbers = new ArrayList<>();
     
         for (int i = 1; i < upperLimit; i++) {
             if (i % 2 == 0) {
-                numbers[index] = i;
-                index++;
+                numbers.add(i);
             }
         }
     
