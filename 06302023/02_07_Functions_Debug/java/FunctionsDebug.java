3979c00b37d7e962567a610f8306c4b5afb34f0a
Peter Hardy <peterphardy@gmail.com>
2023-07-20 21:50:32 -0500

    Two of three functions corrected in java

--- 06302023/02_07_Functions_Debug/java/FunctionsDebug.java
+++ 06302023/02_07_Functions_Debug/java/FunctionsDebug.java
@@ -3,19 +3,25 @@ public class FunctionsDebug {
     public static void main(String[] args) {
         int[] numbers = {8, 2, 5};
         System.out.println(multi(numbers));
+        System.out.println(add(numbers));
     }
 
     public static int multi(int[] numbersList) {
         if (numbersList.length == 0) {
             return 0;
         }
-
         int total = 1;
-
         for (int i = 0; i < numbersList.length; i++) {
             total  *= numbersList[i];
         }
+        return total;
+    }
 
+    public static int add(int[] numbersList) {
+        int total = 0;
+        for (int num : numbersList) {
+            total += num;
+        }
         return total;
     }
 }
