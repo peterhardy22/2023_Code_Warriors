19cf7461ee01d27e5c4d02a1930b7de41727ae8a
Peter Hardy <peterphardy@gmail.com>
2023-07-25 22:08:38 -0500

    Exerrcise completed in java

---
+++ 07143023/42_162_Product/java/Product.java
@@ -0,0 +1,11 @@
+
+public class Product {
+    public static void main(String[] args) {
+        System.out.println(product(2, 2));
+        System.out.println(product(2, -2));
+    }    
+
+    public static int product(int a, int b) {
+        return a * b;
+    }
+}
