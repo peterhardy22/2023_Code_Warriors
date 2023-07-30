f705816376e02808b912e628a5575738faacfe2a
Peter Hardy <peterphardy@gmail.com>
2023-07-29 22:34:36 -0500

    challenge completed in python

---
+++ 07212023/47_215_Built_In_Modules/python/built_in_modules.py
@@ -0,0 +1,6 @@
+# Import the math module:
+import math
+
+# Use math.sqrt to find the square root of 15129 and save it to variable called answer.
+answer: float = math.sqrt(15129)
+print(f"{answer = }")
