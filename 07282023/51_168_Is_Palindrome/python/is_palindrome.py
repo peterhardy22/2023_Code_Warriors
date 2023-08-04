251793e8e13a220d3aded2dbdb5be843b06d5a7b
Peter Hardy <peterphardy@gmail.com>
2023-08-04 11:34:02 -0500

    Challenge completed in python

---
+++ 07282023/51_168_Is_Palindrome/python/is_palindrome.py
@@ -0,0 +1,8 @@
+def is_palindrome(word: str) -> bool:
+    return word == word[::-1]
+
+print(is_palindrome('testing')) # False
+print(is_palindrome('tacocat')) # True
+print(is_palindrome('hannah')) # True
+print(is_palindrome('robert')) # False
+print(is_palindrome('amanaplanacanalpanama')) # True
